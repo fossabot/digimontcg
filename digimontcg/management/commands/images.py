fe0f49c70bf6ac9fcb7e72288394fd6d96985ea5
theandrew168 <andrew@shallowbrooksoftware.com>
2022-11-17 16:40:35 -0600

    Use minio instead of boto3

--- digimontcg/management/commands/images.py
+++ digimontcg/management/commands/images.py
@@ -1,15 +1,28 @@
-import boto3
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from minio import Minio
 
 
 class Command(BaseCommand):
     help = "Replicate card images from database URLs to an S3 bucket"
 
     def add_arguments(self, parser):
-        parser.add_argument("s3_url", help="URL of target S3 bucket")
+        parser.add_argument("s3_endpoint", help="URL of S3 endpoint")
+        parser.add_argument("s3_bucket", help="Name of S3 bucket")
         parser.add_argument("--sets", nargs="+")
 
     def handle(self, *args, **options):
-        client = boto3.client('s3', endpoint_url='http://localhost:9000', aws_access_key_id='minioadmin', aws_secret_access_key='minioadmin')
-        resp = client.list_buckets()
-        print(resp['Buckets'])
+        endpoint = options["s3_endpoint"]
+        bucket = options["s3_bucket"]
+
+        client = Minio(
+            endpoint,
+            access_key="minioadmin",
+            secret_key="minioadmin",
+            secure=False,
+        )
+
+        found = client.bucket_exists(bucket)
+        if not found:
+            client.make_bucket(bucket)
+        else:
+            print(f"Bucket '{bucket}' already exists")
