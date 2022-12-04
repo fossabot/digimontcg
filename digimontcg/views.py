ffb8f7596c23067d5518f989e3e8fcf7e27f77dc
theandrew168 <andrew@shallowbrooksoftware.com>
2022-12-04 13:07:31 -0600

    More layout work, getting closer

--- digimontcg/views.py
+++ digimontcg/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from rest_framework import viewsets
 
 from .models import Card, Set
